7f01c73c784374ecf1a43f5ad428eff63e7e5e82
Dan Fay <dan.c.fay@gmail.com>
2024-01-11 13:26:27 -0700

    Add initial files for code contracts and stats

---
+++ src/lib/codeContracts.ts
@@ -0,0 +1,10 @@
+/**
+ * Total Ways to Sum II
+ *
+ * How many different distinct ways can the number ${n} be written as a sum of integers contained in the set: [${s}]?
+ *
+ * You may use each integer in the set zero or more times.
+ */
+export function solveTotalWaysToSumII(numbers: number[]) {
+  return numbers[0];
+}
---
+++ src/solve-code-contract.ts
@@ -0,0 +1,14 @@
+import { NS } from "@ns";
+import { solveTotalWaysToSumII } from "lib/codeContracts";
+
+export async function main(ns: NS): Promise<void> {
+  const contractType = ns.args[0] as string;
+  let solution: unknown;
+
+  if (contractType === "twts2") {
+    const servers: number[] = JSON.parse(ns.args[1] as string);
+    solution = solveTotalWaysToSumII(servers);
+  }
+
+  ns.tprint(solution);
+}
