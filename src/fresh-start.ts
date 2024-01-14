d0589129f390015c281a56778e1c1342426285f1
Dan Fay <dan.c.fay@gmail.com>
2024-01-13 21:30:53 -0700

    Fresh start is using too much memory to run all 3 scripts. Raise the reserved amount and spawn the last script

--- src/fresh-start.ts
+++ src/fresh-start.ts
@@ -5,10 +5,10 @@ import { nukeServer } from "lib/access";
 export async function main(ns: NS): Promise<void> {
   const script = "basic-money.js";
   const target = "n00dles";
-  const threads = getThreadsForScript(ns, script, "home", 20);
+  const threads = getThreadsForScript(ns, script, "home", 25);
 
   nukeServer(ns, target);
-  ns.run(script, threads, target);
   ns.run("deploy-basic-script.js", 1, script, target, "hackable");
   ns.run("buy-initial-pservs.js", 1, script, target);
+  ns.spawn(script, threads, 1, target);
 }
