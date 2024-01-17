566609f5b6c2d062d9b39bb695eb6cf06e462abc
Dan Fay <dan.c.fay@gmail.com>
2024-01-17 04:33:15 -0700

    Add a baseline of hack threads on 'home'

--- src/setup-ratio-home.ts
+++ src/setup-ratio-home.ts
@@ -4,12 +4,19 @@ import { findBasicMoneyTarget } from "lib/target";
 
 export async function main(ns: NS): Promise<void> {
   const target = (ns.args[0] as string) ?? findBasicMoneyTarget(ns);
-  const totalThreads = getThreadsForScript(ns, "grow.js", "home", 20);
+  const hackThreads = 5;
+  const totalThreads = getThreadsForScript(
+    ns,
+    "grow.js",
+    "home",
+    20 + hackThreads
+  );
   const weakenThreads = Math.floor(totalThreads * 0.2);
   const growThreads = Math.floor(totalThreads * 0.8);
 
   ns.killall("home");
-  ns.run("buy-initial-pservs.js", 1);
+  ns.run("buy-initial-pservs.js");
   ns.run("weaken.js", weakenThreads, target);
+  ns.run("hack.js", hackThreads, target);
   ns.spawn("grow.js", { threads: growThreads, spawnDelay: 1000 }, target);
 }
