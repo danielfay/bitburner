8cbad9362fb431c085d7e17662ec051948942484
Dan Fay <dan.c.fay@gmail.com>
2024-01-22 21:04:25 -0700

    Fix order of if when finding hacking target

--- src/lib/target.ts
+++ src/lib/target.ts
@@ -4,10 +4,10 @@ export function findHackingTarget(ns: NS) {
   let target = "n00dles";
   let hackingSkill = ns.getPlayer().skills.hacking;
 
-  if (hackingSkill > 200) {
-    target = "joesguns";
-  } else if (hackingSkill > 1000) {
+  if (hackingSkill > 1000) {
     target = "phantasy";
+  } else if (hackingSkill > 200) {
+    target = "joesguns";
   }
 
   return target;
