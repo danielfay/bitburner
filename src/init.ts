13652bc86dad6bb84e713399055ab3b1727c653d
Dan Fay <dan.c.fay@gmail.com>
2024-02-04 02:04:11 -0700

    Fix incorrect comparison in init.js

--- src/init.ts
+++ src/init.ts
@@ -12,7 +12,7 @@ export async function main(ns: NS): Promise<void> {
 
   const bitNodeInformation = getBitNodeInformation(ns);
 
-  if ((bitNodeInformation.stage = BitNodeStage.beginning)) {
+  if (bitNodeInformation.stage === BitNodeStage.beginning) {
     await completeStep(ns, "initial-setup.js");
     await completeStep(ns, "get-brutessh.js");
     await completeStep(ns, "backdoor-csec.js");
